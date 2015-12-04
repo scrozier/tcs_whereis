7f25e2f19fcd9a9d75f6c46cd0f4e9dd087b7546
Steve Crozier <steve@crozier.com>
2015-12-04 17:09:39 -0600

    First commit.

---
+++ app/controllers/responder_controller.rb
@@ -0,0 +1,14 @@
+class ResponderController < ApplicationController
+
+  def respond
+    twilio_number = '4694164526'
+    client = Twilio::REST::Client.new 'AC9a67eb2214115a6e1a500dd7aa63ecb5', '2367cdf7e0f18548db85e76c580d5052'
+    response = "Here's lookin' at you, kid."
+    message = client.account.messages.create(
+      from: twilio_number,
+      to: '+12147043209',
+      body: response
+      )
+  end    
+
+end
