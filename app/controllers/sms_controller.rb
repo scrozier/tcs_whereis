59bf72337df6870000bace259c7e712be0b9364a
Steve Crozier <steve@crozier.com>
2015-12-06 20:13:44 -0600

    Implement response.

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -1,20 +1,14 @@
 class SmsController < ApplicationController
-
   skip_before_action :verify_authenticity_token
 
   def incoming
-    render text: "TCS Responder, at your service!"
-  end
-
-  def respond
     twilio_number = '4694164526'
     client = Twilio::REST::Client.new 'AC9a67eb2214115a6e1a500dd7aa63ecb5', '2367cdf7e0f18548db85e76c580d5052'
-    response = "Here's lookin' at you, kid."
+    response = "Here's lookin' at you, #{params["From"]}."
     message = client.account.messages.create(
       from: twilio_number,
-      to: '+12147043209',
+      to: params["From"],
       body: response
       )
-  end    
-
+  end
 end
