e20330f54dcf9eab5b6b4c5df3cc8c0a617ea5c1
Steve Crozier <steve@crozier.com>
2015-12-06 20:39:10 -0600

    Finish most replies.

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -20,8 +20,26 @@ class SmsController < ApplicationController
       'in the vendor conference area (off of reception)'
     when 'data central'
       'in the Information Systems area'
+    when 'perfect product presentation', 'main and main'
+      'at the north end of the Information Systems area'
+    when '1 great = 3 good', 'intuition does not come to an unprepared mind'
+      'off the atrium, behind reception'
+    when 'gumby'
+      "where gumby has always been located...c'mon!"
+    when 'contain yourself'
+      'upstairs, south end, adjacent to CSD'
+    when 'we love our employees'
+      'upstairs, south end, adjacent to CSD (seating area in front of Contain Yourself)'
+    when 'all eyes on elfa'
+      'upstairs, southwest corner'
+    when 'service = selling', 'fun and functional'
+      'upstairs, southwest corner, adjacent to loss prevention'
+    when 'communication is leadership'
+      'upstairs, northwest corner, adjacent to the executive suite'
+    when 'we sell the hard stuff', 'blue sky'
+      'up the stairs, turn right (adjacent to merchandising)'
     else
-      "somewhere, but I don't know where."
+      "somewhere, but I don't know where"
     end
   end
 end
