155c1e83e0432a57c8a8b3f57ad77912f24c1deb
Steve Crozier <steve@crozier.com>
2016-10-03 12:03:55 -0500

    Update conference room list.

--- config/application.rb
+++ config/application.rb
@@ -23,25 +23,26 @@ module TcsWhereis
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    conference_training_center = 'in the Conference/Training Center area, upstairs, south end of building'
+
     config.x.conference_rooms = {
       'fill their baskets' => 'in the vendor conference area (off of reception)',
       'service selection price' => 'in the vendor conference area (off of reception)',
       'man in the desert' => 'in the vendor conference area (off of reception)',
       'air of excitement' => 'in the vendor conference area (off of reception)',
       'data central' => 'in the Information Systems area',
-      'perfect product presentation' => 'at the north end of the Information Systems area',
-      'main and main' => 'at the north end of the Information Systems area',
       '1 great = 3 good' => 'off the atrium, behind reception',
       'intuition does not come to an unprepared mind' => 'off the atrium, behind reception',
       'gumby' => "where gumby has always been located...c'mon!",
-      'contain yourself' => 'upstairs, south end, adjacent to CSD',
-      'we love our employees' => 'upstairs, south end, adjacent to CSD (seating area in front of Contain Yourself)',
-      'all eyes on elfa' => 'upstairs, southwest corner',
-      'service = selling' => 'upstairs, southwest corner, adjacent to loss prevention',
-      'fun and functional' => 'upstairs, southwest corner, adjacent to loss prevention',
+      'contain yourself' => conference_training_center,
+      'we love our employees' => conference_training_center + ' (seating area in front of Contain Yourself)',
+      'all eyes on elfa' => conference_training_center,
+      'service = selling' => conference_training_center,
+      'fun and functional' => conference_training_center,
       'communication is leadership' => 'upstairs, northwest corner, adjacent to the executive suite',
       'we sell the hard stuff' => 'up the stairs, turn right (adjacent to merchandising)',
-      'blue sky' => 'up the stairs, turn right (adjacent to merchandising)'
+      'blue sky' => 'up the stairs, turn right (adjacent to merchandising)',
+      'repetition creates recognition' => 'upstairs, between Loss Prevention and Marketing'
     }
   end
 end
